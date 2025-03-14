608af023fce4f76e4b477765012a44e9df36d686
Reina <20066312@tafe.wa.edu.au>
2025-03-14 17:15:27 +0800

    Chore fixed decryption function

--- substitution.py
+++ substitution.py
@@ -1,6 +1,9 @@
 import random
+from texttable import Texttable
 
 MESSAGE = "the quick brown fox jumps over the lazy dog"
+user_table = Texttable()
+
 
 def shuffle_letters(unshuffled):
     shuffled = unshuffled
@@ -20,6 +23,24 @@ def get_encrypted_alphabet():
     substitution_cipher[" "] = " "
     print(substitution_cipher)
     return substitution_cipher
+
+def init_user_alphabet(origin_dict):
+    user_keys = list(origin_dict.values())
+    user_starting_values = user_keys
+    user_starting_dict = dict(zip(user_keys, user_starting_values))
+    return user_starting_dict
+
+def print_table(user_dict):
+    keys = [letter for letter in list(user_dict.keys())]
+    keys.sort()
+    values = [user_dict[key] for key in keys]
+    user_table.reset()
+    user_table.add_rows([keys[1:14], values[1:14]], False)
+    user_table.add_row(([" "] * 13))
+    user_table.add_rows([keys[14:27], values[14:27]], False)
+    print(user_table.draw())
+    print()
+    return
 
 def encrypt_message(message, cipher_key):
     encrypted_message = ''
@@ -43,10 +64,10 @@ def check_guess(original, guess_message):
 def play():
     substitution_keys = get_encrypted_alphabet()
     cipher_message = encrypt_message(MESSAGE, substitution_keys)
-    guess_keys = dict.fromkeys(list(substitution_keys.values()))
-
+    guess_keys = init_user_alphabet(substitution_keys)
     while True:
         guess_keys = get_guess(guess_keys)
+        print_table(guess_keys)
         current_message = encrypt_message(cipher_message, guess_keys)
         if check_guess(MESSAGE, current_message):
             input("Press enter to quit...  ")
